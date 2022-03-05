9cf96ee377660bb5cac460fffe56a69d0a6b3f3f
Joe Froula <jmfroula@gmail.com>
2022-03-05 10:49:21 -0800

    initial

---
+++ src/lib/Headlines.svelte
@@ -0,0 +1,89 @@
+<script>
+
+    import { onMount } from 'svelte'  
+
+    export let headlines = []
+
+   // const env = process.env.KEY
+
+   // @ts-ignore
+   const key = import.meta.env.VITE_KEY 
+
+    const url = `http://api.mediastack.com/v1/news?access_key=${key}&languages=en`
+
+    onMount( async () => {  
+
+        const res = await fetch(url)    
+        const newsData = await res.json()    
+        headlines = newsData.data   
+        console.log(newsData.data)   
+
+    })
+
+    //Does the same thing as the above but on an event
+
+	// let testFunc = async () => {
+	// 	const res = await fetch(url1) 
+	// 	console.log(res)
+	// 	const data = await res.json()  
+	// 	people1 = data
+	// 	name1 = data[0].name    
+	// 	console.log(res)
+	// 	console.log(process.env.KEY)
+	// }
+
+</script>
+
+<div>
+    
+    
+    <h1>Headlines Daily</h1>
+    {#each headlines as headline (headline.url)}
+        <div id="headline">
+            <img src={headline.image} alt="">
+            <h2>{headline.title}</h2>    
+            <h5>{headline.description}</h5>  
+            <a href={headline.url} target='blank'>Read More</a>   
+            
+        </div>
+    {/each}
+</div>
+
+<style>
+    div {
+        margin-bottom: 50px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        
+    }
+
+    #headline {
+        border: .3px solid lightgray;
+        padding: 40px;
+        border-radius: 15px;
+        background-color: whitesmoke;
+    }
+
+    img {
+        max-height: 50vh;
+        max-width: 30vw;
+        border-radius: 10px;
+        margin-bottom: 15px;
+    }
+
+    h4 {
+        padding-inline: 45px;
+    }
+
+    h1 {
+        margin-bottom: 65px;
+        border: black 1px solid;
+        padding: 15px;
+        border-radius: 35px;
+        background-color: white;
+    }
+
+    
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,38 @@
+<script context="module">
+	export const prerender = true;
+</script>
+
+<script>
+	import Headlines from '$lib/Headlines.svelte'
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Merriweather:wght@300&display=swap" rel="stylesheet">
+</svelte:head>
+
+<main>
+	<Headlines/>  
+</main>
+
+
+<style>
+	main {
+		text-align: center;
+		padding-inline: 30vw;
+		max-width: 240px;
+		margin: 0 auto;
+		background-color: rgb(237, 235, 235);
+		font-family: 'Merriweather', serif;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+
+
+</style>
